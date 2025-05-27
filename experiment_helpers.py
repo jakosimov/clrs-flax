f1123da8392f6660559fab1cdeba37e56b6dfd55
jakosimov <jakobalwall@gmail.com>
2025-05-27 19:51:46 +0100

    Don't apply softmax twice

--- experiment_helpers.py
+++ experiment_helpers.py
@@ -385,9 +385,6 @@ def evaluate_model(
         messages_magnitude = grad_magnitudes[baselines.MESSAGE_LABEL]
 
     message_weights = jnp.array(model.net.processor.mean_message_weights)
-    message_weights = jax.nn.softmax(
-        message_weights[...] / softmax_temperature
-    )  # Ensure weights are normalized
     message_weights = [float(val) for val in message_weights]
     message_weights_dict = {
         f"message_weights_{i}": val for i, val in enumerate(message_weights)
